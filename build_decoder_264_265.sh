fe0230738d9cdf35461e39436796819f81c11ef5
xingquan.hf <xingquan.hf@alibaba-inc.com>
2020-10-22 14:55:24 +0800

    bgr

--- build_decoder_264_265.sh
+++ build_decoder_264_265.sh
@@ -4,7 +4,7 @@ mkdir -p ffmpeg
 cd ../ffmpeg
 make clean
 emconfigure ./configure --cc="emcc" --cxx="em++" --ar="emar" --prefix=$(pwd)/../decoder_wasm/ffmpeg --enable-cross-compile --target-os=none --arch=x86_32 --cpu=generic --ranlib=emranlib \
-    --enable-gpl --enable-version3 --enable-shared \
+    --enable-gpl --enable-version3 --enable-shared --enable-pic \
     --disable-avdevice --disable-avformat --disable-swresample --disable-postproc --disable-avfilter \
     --disable-programs --disable-logging --disable-everything \
     --disable-ffplay --disable-ffprobe --disable-asm --disable-doc --disable-devices --disable-network \
---
+++ build_decoder_wasm_bgr.sh
@@ -0,0 +1,26 @@
+rm -rf dist/libffmpeg2.wasm dist/libffmpeg2.js
+export TOTAL_MEMORY=67108864
+export EXPORTED_FUNCTIONS="[ \
+		'_openDecoder', \
+		'_flushDecoder', \
+		'_closeDecoder', \
+    '_decodeData', \
+    '_main'
+]"
+
+echo "Running Emscripten..."
+emcc decode_video_bgr.c ffmpeg/lib/libavcodec.a ffmpeg/lib/libavutil.a ffmpeg/lib/libswscale.a \
+    -O2 \
+    -pthread \
+    -I "ffmpeg/include" \
+    -s WASM=1 \
+    -s TOTAL_MEMORY=${TOTAL_MEMORY} \
+   	-s EXPORTED_FUNCTIONS="${EXPORTED_FUNCTIONS}" \
+   	-s EXTRA_EXPORTED_RUNTIME_METHODS="['addFunction']" \
+		-s RESERVED_FUNCTION_POINTERS=14 \
+		-s FORCE_FILESYSTEM=1 \
+    -s ASSERTIONS=1 \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -o dist/libffmpeg2.js
+
+echo "Finished Build"
